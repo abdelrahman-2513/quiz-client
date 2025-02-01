879412c3ce9707c31c1f8af000a26a179a8f5dcf
abdelrahman-2513 <mohamed.maher@digi360.tech>
2025-02-01 21:17:09 +0200

    Adjust shown Announcements

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,21 +1,22 @@
-import { Box, CircularProgress,  Typography } from '@mui/material'
+import { Box, CircularProgress, Typography } from '@mui/material';
 import { useAnnouncements } from '../customHooks/useAnnouncemet';
 import AnnouncementBox from './AnnouncementBox';
 
 export default function Announcement() {
-    const { announcements, isLoading: isLoadingAnnouncements } = useAnnouncements();
+  const { announcements, isLoading: isLoadingAnnouncements } = useAnnouncements();
+
   return (
-    <Box sx={{ backgroundColor: "white", width: {md:"60%",sx:"100%"}, padding: 2, borderRadius: 2, height: "100%" }}>
-                    <Typography variant="h5" sx={{ mb: 2 }}>Announcements</Typography>
-                    {isLoadingAnnouncements ? (
-                        <CircularProgress />
-                    ) : announcements?.length > 0 ? (
-                        announcements.map((announcement: any) => (
-                           <AnnouncementBox key={announcement.id} announcement={announcement} />
-                        ))
-                    ) : (
-                        <Typography>No announcements found.</Typography>
-                    )}
-                </Box>
-  )
+    <Box sx={{ backgroundColor: "white", width: { md: "60%", sx: "100%" }, padding: 2, borderRadius: 2, height: "100%" }}>
+      <Typography variant="h5" sx={{ mb: 2 }}>Announcements</Typography>
+      {isLoadingAnnouncements ? (
+        <CircularProgress />
+      ) : announcements?.length > 0 ? (
+        announcements.slice(0, 2).map((announcement: any) => ( // Show only 2 announcements
+          <AnnouncementBox key={announcement.id} announcement={announcement} />
+        ))
+      ) : (
+        <Typography>No announcements found.</Typography>
+      )}
+    </Box>
+  );
 }
